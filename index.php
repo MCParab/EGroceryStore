f1baa015c90c86c3bab9c4eeb23537f2b951eb9f
Mithilesh Parab <parab.mithilesh09@gmail.com>
2016-11-27 23:32:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,83 @@
 
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>EGroceryStore</title>
+        <link rel="stylesheet" href="css/main-style.css" type="text/css"/>
+        <script src="js/jquery-2.1.3.min.js"></script>
+        
+    </head>
+    <body>
+        
+     
+        
+        
+        
+        <header class="Myheader">
+           
+            <div id="menubar">
+                <ul>
+                    <li><a style="width:300px;background-color:#333;margin-right:300px;padding:20px;"> <h1>EGroceryStore</h1></a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="products.php">Products</a></li>
+                    <li><a href="#">About </a></li>
+                    <li><a href="#">Contact </a></li>
+                    <li><a href="signin_form.php">SignUp</a></li>
+                    <li><a href="login.php"  class="login" >Login</a></li>
+                    
+                </ul>
+            </div>
+        </header>
+        
+        
+        <section class="mybackground_section">
+     
+                <div class="logo">
+                    <div><h1>Freshness you can taste. Values you can trust.</h1>
+                    <a href="products.php">Shop Now </a>
+                    </div>
+                </div>
+            
+        
+        </section>
+    
+    
+    <section id="product_section">
+   
+          ]
+        
+    </section>
+    
+    
+    <section id="aboutus_section">
+      
+        <h1>ABOUT US </h1>
+        <p>KiranaBazar is a platform which lets you discover the best products across a wide array of categories that include Grocery, Snacks ,Fruits and Vgetables  and Personal Care. MySmartPrice was founded with the goal of creating the best online shopping research and discovery destination.
+Our price comparison engine enables you in finding the best price, as well as deals and offers from all major Supermarket  stores in India like BigBazar , D_mart and many more .  From the day we started, our aim was to create a unique destination for users looking to buy online, which drives us to focus on one thing above all else: Making it easier for you, the user, to make an informed decision when buying products online.
+Today, more than four million users across India make use of the service on a monthly basis to make their shopping decisions easier. Stay in touch with us , Thank you and Happy Shopping .</p>
+        
+    </section>
+    
+     
+    <section id="contactus_section">
+         <h1>CONTACT US </h1>
+    </section>
+    
+
+     <section id="FollowUs">
+     <h1>Follow us on</h1>
+              <ul>
+              <li><img src="images/facebook.png"/></li>
+              <li><img src="images/twitter.png"/></li>
+              <li><img src="images/google.png"/></li>
+              <li><img src="images/instagram.png"/></li>
+              </ul>
+     </section>
+
+
+    <footer class="footer_section">
+        <h1>Kirana Bazar </h1>
+    </footer>
+    
+    </body>
+</html>
